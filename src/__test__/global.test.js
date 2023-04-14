9980d00bec72f87209d3a79af6d965e3db0deab7
David Esteban Gallego <66768539+Dagapa@users.noreply.github.com>
2023-04-14 18:24:22 -0500

    string, number, boolean test

--- src/__test__/global.test.js
+++ src/__test__/global.test.js
@@ -1,5 +1,27 @@
 const text = "Hola Mundo";
+const fruits = ["manzana", "melon", "banana"];
+const reverseString = (str, callback) => {
+  callback(str.split("").reverse().join(""));
+};
 
 test("Debe contener un texto", () => {
   expect(text).toMatch(/Mundo/);
+});
+
+test("¿Tenemos una banana?", () => {
+  expect(fruits).toContain("banana");
+});
+
+test("Mayor que", () => {
+  expect(10).toBeGreaterThan(9);
+});
+
+test("Verdadero", () => {
+  expect(true).toBeTruthy();
+});
+
+test("Probar un Callback", () => {
+  reverseString("Hola", (str) => {
+    expect(str).toBe("aloH");
+  });
 });
